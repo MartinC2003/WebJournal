2f625dd2688ddea9101abf774cb9f5c361620937
martin <martin.contreras.0302.8@gmail.com>
2025-03-07 17:31:52 -0700

    fixing build

--- application/server/firebase.js
+++
@@ -1,8 +0,0 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('./credentials/webjournal-f20ab-firebase-adminsdk-b6jty-1a364b0b08.json');
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-module.exports = admin;
